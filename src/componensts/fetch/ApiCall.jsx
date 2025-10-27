1c11e5f34fd0566a33563e1ab1ecae967662b320
thaventhiran Ramsan <ramsandota@gmail.com>
2025-10-27 14:12:04 +0530

    create Apicall

---
+++ src/componensts/fetch/ApiCall.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+function ApiCall() {
+  return (
+    <>
+      <div className="content-container">
+        <h1>ApiCall</h1>
+        <div className="content-body">Api call using axios</div>
+      </div>
+
+      <table border={1} cellPadding={5} cellSpacing={0} align="center">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Email</th>
+            <th>isActive</th>
+            <th>CreatedAt</th>
+            <th>UpdatedAt</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr>
+            <td>lkn</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+        </tbody>
+      </table>
+    </>
+  );
+}
+
+export default ApiCall;
+
+
+
+
+
+
